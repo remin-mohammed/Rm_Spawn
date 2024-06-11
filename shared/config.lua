e2f75ed22b71b04def743c91ca4ad0f6c2933a94
Anxxity <reminmohammed123@gmail.com>
2024-06-11 18:40:25 +0530

    Adding config

---
+++ shared/config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+Config.PrimaryColor = 'blue'
+Config.PrimaryShade = 8
+
+Config.Multicharacters = true -- if false. this resource will trigger everytime you spawned in the firsttime
+
+Config.Spawns = {
+	[1] = { name = 'pillbox', label = 'Pillbox Hospital', coord = vector4(246.86, -567.63, 43.28, 242.27), info = 'Pillbox Hill borders Strawberry and Chamberlain Hills to the south.',
+	camera = vector4(265.2420, -644.5110, 68.5038, 14.9216)},
+	[2] = { name = 'mrpd', label = 'Mrpd Police Station', coord = vector4(411.63, -966.19, 29.47, 226.55), info = 'The police station is a modern three-story building, demarcated by Sinner Street, Vespucci Boulevard.',
+	camera = vector4(382.3439, -1065.7062, 51.1300, 319.5745)},
+
+	[3] = { name = 'bennys', label = 'Bennys Garage', coord = vector4(-184.34, -1295.03, 31.3, 127.36), info = 'Bennys Original Motor Works allows players to fit specialized equipment and accessories to their cars ',
+	camera = vector4(-234.6554, -1030.0437, 54.8017, 32.5573)},
+
+	[4] = { name = 'legion', label = 'Legion Square', coord = vector4(223.5, -867.02, 30.49, 11.52), info = 'Legion Square is located in a commercial district,The Square itself is composed primarily of sculptures. ',
+	camera = vector4(168.3882, -797.4686, 37.1142, 200.7812)},
+
+	[5] = { name = 'jobcenter', label = 'Job Center', coord = vector4(-257.46, -981.22, 31.22, 26.14), info = 'a place where the citizen find their dream job',
+	camera = vector4(-306.7475, -974.9052, 50.5610, 310.2451)},
+	[6] = { name = 'cityhall', label = 'City Hall', coord = vector4(-537.59, -217.2, 37.65, 20.88), info = ' The building presumably houses the office of the Mayor and contains many local government agencies',
+	camera = vector4(-528.4287, -276.0943, 51.8954, 10.8677)},
+
+	[7] = { name = 'paleto', label = 'Paleto Garage', coord = vector4(126.39, 6625.41, 31.79, 232.79), info = 'Paleto Bay is located on the lush northern coast of Blaine County, San Andreas. Sitting at the base of the massive Mount Chiliad',
+	camera = vector4(125.9797, 6575.4023, 42.0203, 352.2534)},
+	[8] = { name = 'sandyshore', label = 'Sandy Airstrip', coord = vector4(1747.18, 3274.98, 41.12, 65.92), info = 'Sandy Shores Airfield is a private airfield located on Panorama Drive, southwest of the namesake town of Sandy Shores',
+	camera = vector4(1719.6350, 3236.9556, 48.9503, 327.7537)},
+
+}
